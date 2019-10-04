56066113250b4c007cc14e41935e54ddda9278d1
Kamukama-Daphine <kamdmugisha@gmail.com>
2019-10-04 13:34:58 +0300

    Third commit

---
+++ app/src/main/java/ug/ac/must/bcs/kamfred/MainActivity.java
@@ -0,0 +1,30 @@
+package ug.ac.must.bcs.kamfred;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPersonData();
+            }
+        });
+    }
+    public void openPersonData(){
+        Intent intent = new Intent(this, PersonData.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/ug/ac/must/bcs/kamfred/PersonData.java
@@ -0,0 +1,14 @@
+package ug.ac.must.bcs.kamfred;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_data);
+    }
+}
